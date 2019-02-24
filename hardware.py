b488e006d654db949b3e39c9fc876df725e09074
UweKullmann <kuluwe@web.de>
2019-02-24 14:37:20 +0000

    Add files via upload
    Added ITouchable Callback support

---
+++ hardware.py
@@ -0,0 +1,172 @@
+import time
+from .constants import Return
+from .events import (
+    MsgEvent,
+    StringHeadEvent,
+    NumberHeadEvent,
+    CurrentPageIDHeadEvent,
+    Event
+)
+from .hook import (
+    NexComponents
+)
+
+try:
+    import serial
+    _HAS_PYSERIAL = True
+except ImportError:
+    _HAS_PYSERIAL = False
+
+
+A_END_OF_CMD = [0xff, 0xff, 0xff]
+S_END_OF_CMD = bytearray(A_END_OF_CMD)
+
+
+def _format_cmd(cmd):
+    return bytearray(cmd, encoding="ASCII") + S_END_OF_CMD
+
+
+class AbstractSerialNex:
+    def __init__(self):
+        self.components = NexComponents(self)
+        self.m_touchableList = []
+
+    def write(self, cmd):
+        #print("cmd --> %s" % cmd)
+        cmd = _format_cmd(cmd)
+        #print("cmd --> %s" % cmd)
+        return self.sp.write(cmd)
+
+    def init(self):
+        # mode = Return.Mode.NO_RETURN
+        # mode = Return.Mode.SUCCESS_ONLY  # production setting
+        # mode = Return.Mode.FAIL_ONLY  # default screen setting
+        mode = Return.Mode.ALWAYS  # for debug
+        ret1 = self.set_cmd_response_mode(mode)
+        ret2 = self.send("page 0")
+        if mode in [Return.Mode.ALWAYS, Return.Mode.SUCCESS_ONLY]:
+            return ret1.issuccess() and ret2.issuccess()
+        elif mode in [Return.Mode.FAIL_ONLY, Return.Mode.NO_RETURN]:
+            return ret1.isempty() and ret2.isempty()
+        else:
+            raise NotImplementedError("Undefined mode %s" % mode)
+
+    def set_cmd_response_mode(self, mode):
+        assert isinstance(mode, Return.Mode), "Mode must be a Return.Mode enum"
+        cmd = "bkcmd=%d" % mode.value
+        return self.send(cmd)
+
+    def reset(self):
+        cmd = "rest"
+        # ret = self.send(cmd)
+        self.write(cmd)
+        time.sleep(1)
+        msg = self.read_all()
+        assert msg == b'\x00\x00\x00\xff\xff\xff\x88\xff\xff\xff', "rest returned %s" % msg
+        return True
+
+    def read_all(self):
+        return self.sp.read_all()
+
+    def send(self, cmd):
+        self.write(cmd)
+        time.sleep(0.1)
+        msg = self.read_all()
+        #print("msg <-- %s" % msg)
+        evt = MsgEvent.parse(msg)
+        #print("msg <-- %s" % evt)
+        return evt
+
+    def get_nex_string_command(self, cmd):
+        evt = self.send(cmd)
+        assert isinstance(evt, StringHeadEvent)
+        return evt.value
+
+    def set_nex_string_command(self, cmd):
+        return self.send(cmd)
+
+    def get_nex_number_command(self, cmd, signed, bit_size):
+        evt = self.send(cmd)
+        assert isinstance(evt, NumberHeadEvent)
+        if signed:
+            return evt.signed_value
+        else:
+            return evt.value
+
+    def set_nex_number_command(self, cmd):
+        return self.send(cmd)
+
+    def get_nex_bool_command(self, cmd):
+        evt = self.send(cmd)
+        assert isinstance(evt, NumberHeadEvent)
+        return bool(evt.value)
+
+    def set_nex_bool_command(self, cmd):
+        return self.send(cmd)
+
+    @property
+    def current_page(self):
+        cmd = "sendme"
+        evt = self.send(cmd)
+        assert isinstance(evt, CurrentPageIDHeadEvent)
+        return evt.pid
+
+    def close(self):
+        return self.sp.close()
+
+    def poll(self):
+        #print("poll")
+        data = self.read_all()
+        print("data: %s" % data)
+        if len(data) == 7:
+            if data[0] == Return.Code.EVENT_TOUCH_HEAD.value: #Touchevent received
+                if data[4] == 0xFF and data[5] == 0xFF and data[6] == 0xFF:
+                    for Touchable in self.m_touchableList:
+                        Touchable.process_event(data[1], data[2], Event.Touch(data[3]))
+
+    def registerTouchable(self, touchable):
+        print('register touch')
+        self.m_touchableList.append(touchable)
+
+
+
+
+if _HAS_PYSERIAL:
+    class PySerialNex(AbstractSerialNex):
+        def __init__(self, *args, **kwargs):
+            super().__init__()
+            self.sp = serial.Serial(*args, **kwargs)
+
+
+class NexSerialMock(AbstractSerialNex):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def write(self, cmd):
+        pass
+
+    def read(self):
+        return None
+
+    def close(self):
+        #print("close")
+        return None
+
+
+"""
+# PyBoard 1.1
+# https://docs.micropython.org/en/latest/pyboard/pyboard/quickref.html
+# RED: VIN
+# BLACK: GND
+# YELLOW: X9 (Board TX)
+# BLUE: X10 (Board RX)
+
+import machine
+import time
+
+
+class uPyNexSerial(AbstractSerialNex):
+    def __init__(self, *args, **kwargs):
+        self.sp = machine.UART(*args, **kwargs)
+
+"""
---
+++ interfaces.py
@@ -0,0 +1,222 @@
+from .constants import (
+    Alignment
+)
+
+from .resources import (
+    Font,
+    Picture
+)
+from .events import Event
+
+
+class NxInterface:
+    def _send(self, cmd):
+        return self._nid._nexserial.send(cmd)
+
+    def _get_nex_boolean_property(self, prop):
+        name = self._nid.name
+        cmd = "get %s.%s" % (name, prop)
+        return self._nid._nexserial.get_nex_bool_command(cmd)
+
+    def _set_nex_boolean_property(self, prop, value):
+        name = self._nid.name
+        if value:
+            value = 1
+        else:
+            value = 0
+        cmd = "%s.%s=%s" % (name, prop, value)
+        return self._nid._nexserial.set_nex_bool_command(cmd)
+
+    def _get_nex_number_property(self, prop, signed, bit_size):
+        name = self._nid.name
+        cmd = "get %s.%s" % (name, prop)
+        return self._nid._nexserial.get_nex_number_command(cmd, signed, bit_size)
+
+    def _set_nex_number_property(self, prop, value):
+        name = self._nid.name
+        cmd = "%s.%s=%s" % (name, prop, value)
+        return self._nid._nexserial.set_nex_number_command(cmd)
+
+    def _get_nex_string_property(self, prop):
+        name = self._nid.name
+        cmd = "get %s.%s" % (name, prop)
+        return self._nid._nexserial.get_nex_string_command(cmd)
+
+    def _set_nex_string_property(self, prop, value):
+        name = self._nid.name
+        cmd = "%s.%s=\"%s\"" % (name, prop, value)
+        return self._nid._nexserial.set_nex_string_command(cmd)
+
+
+class INumericalUnsignedValued(NxInterface):
+    @property
+    def value(self):
+        return self._get_nex_number_property("val", False, 32)
+
+    @value.setter
+    def value(self, value):
+        self._set_nex_number_property("val", value)
+
+
+class INumericalSignedValued(NxInterface):
+    @property
+    def value(self):
+        return self._get_nex_number_property("val", True, 32)
+
+    @value.setter
+    def value(self, value):
+        self._set_nex_number_property("val", value)
+
+
+class IBooleanValued(NxInterface):
+    @property
+    def value(self):
+        # return bool(self.get_nex_number_property("val", False, 32))
+        return self._get_nex_boolean_property("val")
+
+    @value.setter
+    def value(self, value):
+        value = bool(value)
+        self._set_nex_boolean_property("val", value)
+
+
+class IStringValued(NxInterface):
+    @property
+    def text(self):
+        return self._get_nex_string_property("txt")
+
+    @text.setter
+    def text(self, value):
+        self._set_nex_string_property("txt", value)
+
+
+class IColourable(NxInterface):
+    @property
+    def backcolor(self):
+        return self._get_nex_number_property("bco", False, 32)
+
+    @backcolor.setter
+    def backcolor(self, color):
+        self._set_nex_number_property("bco", color.value)
+
+    @property
+    def forecolor(self):
+        return self._get_nex_number_property("pco", False, 32)
+
+    @forecolor.setter
+    def forecolor(self, color):
+        self._set_nex_number_property("pco", color.value)
+
+
+class AlignmentDirection(NxInterface):
+    def __init__(self, nid):
+        self._nid = nid
+
+    @property
+    def vertical(self):
+        return Alignment.Vertical(self._get_nex_number_property("ycen", False, 32))
+
+    @vertical.setter
+    def vertical(self, value):
+        assert isinstance(value, Alignment.Vertical), "Argument must be %r" % Alignment.Vertical
+        self._set_nex_number_property("ycen", value.value)
+
+    @property
+    def horizontal(self):
+        return Alignment.Horizontal(self._get_nex_number_property("xcen", False, 32))
+
+    @horizontal.setter
+    def horizontal(self, value):
+        assert isinstance(value, Alignment.Horizontal), "Argument must be %r" % Alignment.Horizontal
+        self._set_nex_number_property("xcen", value.value)
+
+
+class IFontStyleable(NxInterface):
+    @property
+    def font(self):
+        return self._get_nex_number_property("font", False, 32)
+
+    @font.setter
+    def font(self, value):
+        assert isinstance(value, Font), "Argument must be %r" % Font
+        self._set_nex_number_property("font", value.id)
+
+    @property
+    def alignment(self):
+        return AlignmentDirection(self._nid)
+
+
+class IPicturable(NxInterface):
+    @property
+    def picture(self):
+        return self._get_nex_number_property("pic", False, 32)
+
+    @picture.setter
+    def picture(self, value):
+        assert isinstance(value, Picture), "Argument must be %r" % Picture
+        self._set_nex_number_property("pic", value.id)
+
+
+class IViewable(NxInterface):
+    @property
+    def visible(self):
+        oid = self.nid.name
+        return self._send("vis %s" % oid)
+
+    @visible.setter
+    def visible(self, value):
+        oid = self._nid.name
+        if value:
+            cmd = "vis %s,1" % oid
+        else:
+            cmd = "vis %s,0" % oid
+        self._send(cmd)
+
+
+class IHeightable(NxInterface):
+    @property
+    def height(self):
+        return self._get_nex_number_property("hig", False, 32)
+
+    @height.setter
+    def height(self, value):
+        self._set_nex_number_property("hig", value)
+
+
+class IWidthable(NxInterface):
+    @property
+    def width(self):
+        return self._get_nex_number_property("wid", False, 32)
+
+    @width.setter
+    def width(self, value):
+        self._set_nex_number_property("wid", value)
+
+
+class ITouchable(NxInterface):
+    _callback = None
+
+    @property
+    def callback(self):
+        return self._callback
+
+    @callback.setter
+    def callback(self, func):
+        #print("set callback %s" % func)
+        self._callback = func
+
+    def process_event(self, pid, cid, event_type):
+        assert isinstance(event_type, Event.Touch)
+
+        if pid != self._nid.pid:
+            return False
+
+        if cid != self._nid.cid:
+            return False
+
+        if event_type in [Event.Touch.Press, Event.Touch.Release]:
+            if self.callback is not None:
+                self.callback(self, event_type)
+                return True
+
+        return False
